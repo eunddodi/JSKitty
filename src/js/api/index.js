38ee968117371446180f836922c87756af8e0dbd
eunddodi <eundppojjack@gmail.com>
2022-09-19 09:47:38 +0900

    fix: Change baseurl

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  'https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev';
+  'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev';
 
 const CatApi = {
   async fetchData(nodeId) {
